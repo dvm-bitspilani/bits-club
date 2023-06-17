9bf692adadb96a9d43bd4a1b520dad67b899ec3e
yoshicon <shreyaskgantayet908@gmail.com>
2023-06-17 17:09:41 +0530

    minor changes

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -1,9 +1,16 @@
 import { Link } from "react-router-dom"
 
+const LIST_STYLES = {
+    listStyle : "none",
+    display : "flex",
+    justifyContent:"space-evenly",
+    width:"40%"
+}
+
 export default function NavBar() {
     return (
         <nav>
-            <ul style={{listStyle : "none",display : "flex", justifyContent:"space-evenly", width:"40"}}>
+            <ul style={LIST_STYLES}>
                 <li><Link to = '/'> Home </Link></li>
                 <li><Link to = '/club'> Club </Link></li>
             </ul>
