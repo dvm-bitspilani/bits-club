643a1ea36f3b0654a2e7c3d88077b8f7357d0e44
yoshicon <shreyaskgantayet908@gmail.com>
2023-06-17 11:26:57 +0530

    Centered the nav. Added useParams in clubsPage, changed the club paths to a dynamic one

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
       element: <LandingPage/>,
     },
     {
-      path: "/club",
+      path: "/:club",
       element: <ClubPage/>,
     }
   ])
--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom"
 export default function NavBar() {
     return (
         <nav>
-            <ul>
+            <ul style={{listStyle : "none",display : "flex", justifyContent:"space-evenly", width:"40"}}>
                 <li><Link to = '/'> Home </Link></li>
                 <li><Link to = '/club'> Club </Link></li>
             </ul>
