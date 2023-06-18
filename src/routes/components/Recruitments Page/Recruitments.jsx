32f32cc908d1ddd1d64dc4af8f2ce891d7aa6ffb
Akshit <f20220429@pilani.bits-pilani.ac.in>
2023-06-18 21:35:03 +0530

    recruitments page final layout

--- src/routes/components/Recruitments Page/Recruitments.jsx
+++ src/routes/components/Recruitments Page/Recruitments.jsx
@@ -1,23 +1,28 @@
 import React from "react"
 import "./Recruitments.css"
 import Task from "./Task.jsx"
+import ImgContainer1 from "./ImgContainer1"
 
-export default function RecruitmentsPage(){
-    return(
-        <div className='recruitments'>
+export default function RecruitmentsPage() {
+    return (
+        <div className='recruitments' style = {{backgroundImage: "url(../src/assets/Sprinkle2.png)"}}>
             <div className='rec-process'>
                 <h1 className="rec-clubname">ACM Recruitments</h1>
-                <h2 className="heading-1">Recruitment Process</h2>
-                <Task/>
-                <Task/>
+                <div className="button-container">
+                    <h2 className="heading-1">Recruitment Process</h2>
+                    <button>Go to Club Page</button>
+                </div>
+                <Task />
+                <Task />
                 <h2 className="heading-1">Recruitment Form</h2>
             </div>
 
             <div className="image-section">
-                <img src="src\assets\laptop.png" alt="image not found"/>
+                <ImgContainer1 />
                 <p className="text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos sint sunt inventore rerum! Iure soluta quos quas adipisci quaerat consequatur?</p>
-                <button>Go to Club Page</button>
             </div>
         </div>
     )
 }
+
+
