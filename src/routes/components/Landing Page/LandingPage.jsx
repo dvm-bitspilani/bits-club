7926e208b32b663cfb861300bdd7ea28b26be663
Ritikchandra <ritik.chandra009@gmail.com>
2023-06-23 00:49:34 +0530

    add: scroll snap

--- src/routes/components/Landing Page/LandingPage.jsx
+++ src/routes/components/Landing Page/LandingPage.jsx
@@ -1,4 +1,6 @@
 import './LandingPage.css'
+import './ClubCard.jsx'
+import ClubCard from './ClubCard.jsx'
 export default function LandingPage() {
     return (
         <>
@@ -32,15 +34,15 @@ export default function LandingPage() {
                             </button>
                         </div>
                     </div>
-                    <div className="first-landing-page-requirements">
-                    <div className="landing-page-requirements-wrapper">
-                            <span className="landing-page-requirements-text">
+                    <div className="first-landing-page-recruitments">
+                    <div className="landing-page-recruitments-wrapper">
+                            <span className="landing-page-recruitments-text">
                                 Events
                             </span>
-                            <p className='landing-page-requirements-para'>
+                            <p className='landing-page-recruitments-para'>
                             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut euismod odio a ipsum vehicula semper sed imperdiet nunc. Integer varius tortor vel mauris 
                             </p>
-                            <button className="first-landing-page-requirements-explore-btn">
+                            <button className="first-landing-page-recruitments-explore-btn">
                                 Explore
                             </button>
                         </div>
@@ -49,7 +51,26 @@ export default function LandingPage() {
             </div>
         </div>
         <div className="second-landing-page">
-            
+        <div className="second-landing-page-wrapper">
+            <div className="second-landing-page-links">
+               <a href="#" id='second-landing-page-clubs-link'>Clubs</a>
+               <a href="#" id='second-landing-page-departments-link'>Departments</a>
+            </div>
+            <div className="second-landing-page-cards-wrapper">
+               <ClubCard clubName = 'ACM' role = 'Technical' skill='Coding'/>
+               <ClubCard clubName = 'ACM' role = 'Technical' skill='Coding'/>
+               <ClubCard clubName = 'ACM' role = 'Technical' skill='Coding'/>
+               <ClubCard clubName = 'ACM' role = 'Technical' skill='Coding'/>
+               <ClubCard clubName = 'ACM' role = 'Technical' skill='Coding'/>
+               <ClubCard clubName = 'ACM' role = 'Technical' skill='Coding'/>
+               <ClubCard clubName = 'ACM' role = 'Technical' skill='Coding'/>
+               <ClubCard clubName = 'ACM' role = 'Technical' skill='Coding'/>
+               <ClubCard clubName = 'ACM' role = 'Technical' skill='Coding'/>
+               <ClubCard clubName = 'ACM' role = 'Technical' skill='Coding'/>
+               <ClubCard clubName = 'ACM' role = 'Technical' skill='Coding'/>
+               <ClubCard clubName = 'ACM' role = 'Technical' skill='Coding'/>
+            </div>
+        </div>
         </div>
     </>
     )
