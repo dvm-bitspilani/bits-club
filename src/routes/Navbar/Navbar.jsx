c77d9cd9e78a59703149644fda814e6000fcde50
Ritikchandra <ritik.chandra009@gmail.com>
2023-06-23 05:56:58 +0530

    add: search bar

--- src/routes/Navbar/Navbar.jsx
+++ src/routes/Navbar/Navbar.jsx
@@ -1,8 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./Navbar.css";
 import image from "../../Assets/Vector(1).png";
 import { Link } from "react-router-dom";
 export default function Navbar() {
+  const openSearchInputBox = ()=>{
+      document.getElementById('navbar-search-input-item').classList.toggle('hiddenDisplay');
+      document.getElementById('navbar-test-item').classList.toggle('hiddenDisplay');
+      document.getElementById('navbar-clubs-item').classList.toggle('hiddenDisplay');
+      document.getElementById('navbar-recruitments-item').classList.toggle('hiddenDisplay');
+      document.getElementById('navbar-profile-item').classList.toggle('hiddenDisplay');
+      document.getElementsByClassName('navbar-logo')[0].classList.toggle('hiddenDisplay');
+      document.getElementsByClassName('hamburger-menu-btn')[0].classList.toggle('hiddenDisplay');
+  }
+  // window.addEventListener('scroll', ()=>{
+  //   if(window.scrollY>0){
+  //     document.querySelector('.navbar-right').classList.remove('hiddenDisplay')
+  //   }
+  //   else{
+  //     document.querySelector('.navbar-right').classList.add('hiddenDisplay')
+  //   }
+  // })
   return (
     <>
       <nav>
@@ -13,7 +30,7 @@ export default function Navbar() {
               <span className="hamline" id="hamline2"></span>
               <span className="hamline" id="hamline3"></span>
             </div>
-            <div className="navbar-logo">
+            <div className = 'navbar-logo'>
               <Link to="/">
                 <span id="navbar-logo-bits">BITS</span>
                 <span id="navbar-logo-clubs">Clubs</span>
@@ -22,22 +39,25 @@ export default function Navbar() {
           </div>
           <div className="navbar-right">
             <ul className="navbar-right-list">
-              <li>
+              <li id='navbar-test-item'>
                 <Link to="/Association of Computing Machinery">[TEST ONLY] CLUB PAGE (ACM for now)</Link>
               </li>
-              <li>
+              <li id='navbar-clubs-item'>
                 <Link to="/club">CLUBS</Link>
               </li>
-              <li>
+              <li id='navbar-recruitments-item'>
                 <Link to="/(club-name)/recruitments">RECRUITMENTS</Link>
               </li>
-              <li>
+              <li id='navbar-profile-item'>
                 <Link to="/">MY PROFILE</Link>
               </li>
-              <li>
-                <a href="#">
-                  <img src={image} alt="" width="15px" />
+              <li id="navbar-search-btn-item">
+                <a href="#" >
+                  <img src={image} alt="" width="15px" onClick={openSearchInputBox}/>
                 </a>
+              </li>
+              <li id="navbar-search-input-item" className="hiddenDisplay"> 
+                <input type="text" placeholder="Search" className="navbar-search-input-box"/>
               </li>
             </ul>
           </div>
