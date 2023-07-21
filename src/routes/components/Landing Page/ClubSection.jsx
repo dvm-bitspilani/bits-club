c90338043e8e1c73160ae7ee944934623f9695f6
Ritikchandra <ritik.chandra009@gmail.com>
2023-07-21 21:34:20 +0530

    new: dept/club link

---
+++ src/routes/components/Landing Page/ClubSection.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import ClubCard from './ClubCard.jsx'
+export default function ClubSection() {
+  return (
+    <>
+               <ClubCard clubName = 'ACM' role = 'Technical' skill='Coding'/>
+               <ClubCard clubName = 'ACM' role = 'Technical' skill='Coding'/>
+               <ClubCard clubName = 'ACM' role = 'Technical' skill='Coding'/>
+               <ClubCard clubName = 'ACM' role = 'Technical' skill='Coding'/>
+               <ClubCard clubName = 'ACM' role = 'Technical' skill='Coding'/>
+               <ClubCard clubName = 'ACM' role = 'Technical' skill='Coding'/>
+               <ClubCard clubName = 'ACM' role = 'Technical' skill='Coding'/>
+               <ClubCard clubName = 'ACM' role = 'Technical' skill='Coding'/>
+               <ClubCard clubName = 'ACM' role = 'Technical' skill='Coding'/>
+               <ClubCard clubName = 'ACM' role = 'Technical' skill='Coding'/>
+               <ClubCard clubName = 'ACM' role = 'Technical' skill='Coding'/>
+               <ClubCard clubName = 'ACM' role = 'Technical' skill='Coding'/>
+    </>
+  )
+}
---
+++ src/routes/components/Landing Page/DeptSection.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import ClubCard from './ClubCard.jsx'
+export default function ClubSection() {
+  return (
+    <>
+               <ClubCard clubName = 'DVM' role = 'Technical' skill='Coding'/>
+               <ClubCard clubName = 'DVM' role = 'Technical' skill='Coding'/>
+               <ClubCard clubName = 'DVM' role = 'Technical' skill='Coding'/>
+               <ClubCard clubName = 'DVM' role = 'Technical' skill='Coding'/>
+               <ClubCard clubName = 'DVM' role = 'Technical' skill='Coding'/>
+               <ClubCard clubName = 'DVM' role = 'Technical' skill='Coding'/>
+               <ClubCard clubName = 'DVM' role = 'Technical' skill='Coding'/>
+               <ClubCard clubName = 'DVM' role = 'Technical' skill='Coding'/>
+               <ClubCard clubName = 'DVM' role = 'Technical' skill='Coding'/>
+               <ClubCard clubName = 'DVM' role = 'Technical' skill='Coding'/>
+               <ClubCard clubName = 'DVM' role = 'Technical' skill='Coding'/>
+               <ClubCard clubName = 'DVM' role = 'Technical' skill='Coding'/>
+    </>
+  )
+}
