a9126307619f621ba0c1fd28ebcd4cbeac7c47c6
yoshicon <shreyaskgantayet908@gmail.com>
2023-07-23 21:18:29 +0530

    Skill Editing Added, Linked to the server, Image Upload also Added TBA : A proper way to edit and add the skill tags       A custom drag and drop image Upload       Showing the users that the image is uploading       Editing the club Image       Another way to edit the club description

--- src/routes/components/Club Page/components/Modal/EventAddModal.jsx
+++ src/routes/components/Club Page/components/Modal/EventAddModal.jsx
@@ -1,17 +1,47 @@
 import editModal from "./EventEditModal.module.css";
 
 import { createPortal } from "react-dom";
+import { useState } from "react";
+import axios from "axios";
 
 export default function EventAddModal({ onClose, handleAddEvent }) {
+  const [imgPath, setImgPath] = useState("");
 
   const handleSubmit = (e) => {
+    if (e.target[0].value === "" || e.target[1].value === "") {
+      alert("Please fill in all fields");
+      return;
+    }
+    if (imgPath === "") {
+      alert("Please upload an image or Wait for the image to upload");
+      return;
+    }
+    if (confirm("Are you sure you want to add this event?") === null) {
+      return;
+    }
     e.preventDefault();
     const name = e.target[0].value;
     const description = e.target[1].value;
-    handleAddEvent(name, description);
+    handleAddEvent(name, description, imgPath);
     onClose();
   };
 
+  const handleImageUpload = (e) => {
+    e.preventDefault();
+    const image = e.target.files[0];
+    const formData = new FormData();
+    formData.append("image", image);
+    axios
+      .post("https://bits-clubs.onrender.com/api/v1/uploadImage", formData)
+      .then((res) => {
+        setImgPath(res.data.img_path);
+        alert("Image Uploaded Successfully");
+      })
+      .catch((err) => {
+        console.error(err);
+        alert("Image Upload Failed");
+      });
+  };
 
   return (
     <>
@@ -23,9 +53,34 @@ export default function EventAddModal({ onClose, handleAddEvent }) {
               X
             </button>
             <form className={editModal.form} onSubmit={handleSubmit}>
-              <label htmlFor={editModal.inputName} className={editModal.label}>Event Name  </label> <input id={editModal.inputName} placeholder="Enter the name of event" type="text" />
-              <label htmlFor={editModal.textarea} className={editModal.label}>Event Description  </label><textarea placeholder="Enter the description of the event" id={editModal.textarea}/>
-              <button className={editModal.submitButton} type="submit">Submit</button>
+              <label htmlFor={editModal.inputName} className={editModal.label}>
+                Event Name{" "}
+              </label>{" "}
+              <input
+                id={editModal.inputName}
+                placeholder="Enter the name of event"
+                type="text"
+                encType="multipart/form-data"
+              />
+              <label htmlFor={editModal.textarea} className={editModal.label}>
+                Event Description{" "}
+              </label>
+              <textarea
+                placeholder="Enter the description of the event"
+                id={editModal.textarea}
+              />
+              <label htmlFor={editModal.inputImage} className={editModal.label}>
+                Event Image{" "}
+              </label>{" "}
+              <input
+                name="uploaded_file"
+                id={editModal.inputImage}
+                type="file"
+                onChange={handleImageUpload}
+              />
+              <button className={editModal.submitButton} type="submit">
+                Submit
+              </button>
             </form>
           </div>
         </div>,
--- src/routes/components/Club Page/components/Modal/EventEditModal.jsx
+++ src/routes/components/Club Page/components/Modal/EventEditModal.jsx
@@ -1,11 +1,18 @@
 import editModal from "./EventEditModal.module.css";
 import { createPortal } from "react-dom";
+import { useState } from "react";
+import axios from "axios";
 
 export default function EventEditModal({ onClose, event, handleEditEvent }) {
-
+  const [imgPath, setImgPath] = useState("");
+  
   const handleSubmit = (e) => {
     if (e.target[0].value === "" || e.target[1].value === "") {
       alert("Please fill in all fields");
+      return;
+    }
+    if (imgPath === "") {
+      alert("Please upload an image or Wait for the image to upload");
       return;
     }
     if (confirm("Are you sure you want to edit this event?") === null) {
@@ -14,10 +21,26 @@ export default function EventEditModal({ onClose, event, handleEditEvent }) {
     e.preventDefault();
     const name = e.target[0].value;
     const description = e.target[1].value;
-    handleEditEvent(event, name, description);
+    handleEditEvent(event, name, description, imgPath);
     onClose();
   };
 
+  const handleImageUpload = (e) => {
+    e.preventDefault();
+    const image = e.target.files[0];
+    const formData = new FormData();
+    formData.append("image", image);
+    axios
+      .post("https://bits-clubs.onrender.com/api/v1/uploadImage", formData)
+      .then((res) => {
+        setImgPath(res.data.img_path);
+        alert("Image Uploaded Successfully");
+      })
+      .catch((err) => {
+        console.error(err);
+        alert("Image Upload Failed");
+      });
+  };
 
   return (
     <>
@@ -28,10 +51,38 @@ export default function EventEditModal({ onClose, event, handleEditEvent }) {
             <button className={editModal.deleteButton} onClick={onClose}>
               X
             </button>
-            <form className={editModal.form} onSubmit={handleSubmit}>
-              <label htmlFor={editModal.inputName} className={editModal.label}>Event Name  </label> <input id={editModal.inputName} defaultValue={event.name} type="text" />
-              <label htmlFor={editModal.textarea} className={editModal.label}>Event Description  </label><textarea defaultValue={event.description} id={editModal.textarea}/>
-              <button className={editModal.submitButton} type="submit">Submit</button>
+            <form
+              className={editModal.form}
+              onSubmit={handleSubmit}
+              encType="multipart/form-data"
+            >
+              <label htmlFor={editModal.inputName} className={editModal.label}>
+                Event Name{" "}
+              </label>{" "}
+              <input
+                id={editModal.inputName}
+                defaultValue={event.name}
+                type="text"
+              />
+              <label htmlFor={editModal.textarea} className={editModal.label}>
+                Event Description{" "}
+              </label>
+              <textarea
+                defaultValue={event.description}
+                id={editModal.textarea}
+              />
+              <label htmlFor={editModal.inputImage} className={editModal.label}>
+                Event Image{" "}
+              </label>{" "}
+              <input
+                name="uploaded_file"
+                id={editModal.inputImage}
+                type="file"
+                onChange={handleImageUpload}
+              />
+              <button className={editModal.submitButton} type="submit">
+                Submit
+              </button>
             </form>
           </div>
         </div>,
