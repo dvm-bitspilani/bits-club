2364c82946f5ce58a608ca9f91c82536de979fca
Ritikchandra <ritik.chandra009@gmail.com>
2023-07-25 00:09:33 +0530

    modified: many changes

--- src/routes/components/Landing Page/ClubCard.jsx
+++ src/routes/components/Landing Page/ClubCard.jsx
@@ -15,15 +15,15 @@
 // }
 
 import React from 'react';
-
+import { Link } from 'react-router-dom';
 export default function ClubCard(props) {
   return (
-    <div className="landing-page-card" style={{ backgroundImage: `url(${props.img})` }}>
+    <Link to="/searchpage" className="landing-page-card" style={{ backgroundImage: `url(${props.img})` }}>
       <span className="landing-page-card-club-name">{props.clubName}</span>
       <div className="role-skill-wrapper">
         <span className="landing-page-card-role-type">{props.role}</span>
         <span className="landing-page-card-skill-name">{props.skill}</span>
       </div>
-    </div>
+    </Link>
   );
 }
