6eb23a5338f85dae080321983c57b6150f40ca34
Akshit <f20220429@pilani.bits-pilani.ac.in>
2023-07-31 17:16:55 +0530

    build: event modals

--- src/routes/components/Events Page/Upcoming.jsx
+++ src/routes/components/Events Page/Upcoming.jsx
@@ -1,16 +1,16 @@
+import React from 'react';
 import Marquee from "react-fast-marquee";
+import "./EventsPage.css";
 
-
-export default function Upcoming(props) {
-
-    const gap = "⠀⠀⠀⠀⠀"
+export default function Upcoming({ title, onClick }) {
+    const gap = "⠀⠀⠀⠀⠀";
 
     return (
-        <div>
-            <Marquee className="events-upcoming-card" pauseOnHover="true" speed={100} autoFill="true">
-                <h3 className="events-upcoming-card-text">{props.title}</h3>
+        <div onClick={onClick}>
+            <Marquee className="events-upcoming-card" pauseOnHover={true} speed={100} autoFill={true}>
+                <h3 className="events-upcoming-card-text">{title}</h3>
                 <h3 className="events-upcoming-card-text">{gap}</h3>
             </Marquee>
         </div>
-    )
+    );
 }
